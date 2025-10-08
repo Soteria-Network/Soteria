ce60a0fa2a22c35b41f5b6105af1b54e66c41f11
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2025-10-08 17:55:25 +0300

    Bump ProtocolV

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static constexpr int PROTOCOL_VERSION = 70034;
+static constexpr int PROTOCOL_VERSION = 70035;
 
 //! initial proto version, to be increased after version/verack negotiation
 static constexpr int INIT_PROTO_VERSION = 209;
