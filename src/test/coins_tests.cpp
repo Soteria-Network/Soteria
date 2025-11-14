6652042d0009ca749b40b39c7241b4d392c36886
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2025-11-14 18:52:36 +0300

    Update coins_tests.cpp

--- src/test/coins_tests.cpp
+++ src/test/coins_tests.cpp
@@ -96,9 +96,9 @@ namespace
             // Manually recompute the dynamic usage of the whole data, and compare it.
             size_t ret = memusage::DynamicUsage(cacheCoins);
             size_t count = 0;
-            for (CCoinsMap::iterator it = cacheCoins.begin(); it != cacheCoins.end(); it++)
+            for (const auto& entry : cacheCoins)
             {
-                ret += it->second.coin.DynamicMemoryUsage();
+                ret += entry.second.coin.DynamicMemoryUsage();
                 ++count;
             }
             BOOST_CHECK_EQUAL(GetCacheSize(), count);
@@ -219,18 +219,17 @@ BOOST_FIXTURE_TEST_SUITE(coins_tests, BasicTestingSetup)
             // Once every 1000 iterations and at the end, verify the full cache.
             if (InsecureRandRange(1000) == 1 || i == NUM_SIMULATION_ITERATIONS - 1)
             {
-                for (auto it = result.begin(); it != result.end(); it++)
-                {
-                    bool have = stack.back()->HaveCoin(it->first);
-                    const Coin &coin = stack.back()->AccessCoin(it->first);
+                for (const auto& entry : result) {
+                    bool have = stack.back()->HaveCoin(entry.first);
+                    const Coin &coin = stack.back()->AccessCoin(entry.first);
                     BOOST_CHECK(have == !coin.IsSpent());
-                    BOOST_CHECK(coin == it->second);
+                    BOOST_CHECK(coin == entry.second);
                     if (coin.IsSpent())
                     {
                         missed_an_entry = true;
                     } else
                     {
-                        BOOST_CHECK(stack.back()->HaveCoinInCache(it->first));
+                        BOOST_CHECK(stack.back()->HaveCoinInCache(entry.first));
                         found_an_entry = true;
                     }
                 }
@@ -481,12 +480,12 @@ BOOST_FIXTURE_TEST_SUITE(coins_tests, BasicTestingSetup)
             // Once every 1000 iterations and at the end, verify the full cache.
             if (InsecureRandRange(1000) == 1 || i == NUM_SIMULATION_ITERATIONS - 1)
             {
-                for (auto it = result.begin(); it != result.end(); it++)
+                for (const auto& entry : result)
                 {
-                    bool have = stack.back()->HaveCoin(it->first);
-                    const Coin &coin = stack.back()->AccessCoin(it->first);
+                    bool have = stack.back()->HaveCoin(entry.first);
+                    const Coin& coin = stack.back()->AccessCoin(entry.first);
                     BOOST_CHECK(have == !coin.IsSpent());
-                    BOOST_CHECK(coin == it->second);
+                    BOOST_CHECK(coin == entry.second);
                 }
             }
 
