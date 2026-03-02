f01c706b7c3befd20b728995e019ce1c1b505faf
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-03-03 01:52:09 +0300

    increasing security

--- src/chain.h
+++ src/chain.h
@@ -22,10 +22,10 @@
  * Maximum amount of time that a block timestamp is allowed to exceed the
  * current network-adjusted time before the block will be accepted.
  */
-// increase security against timestamp manipulation
-static const int64_t MAX_FUTURE_BLOCK_TIME = 180; 
-static const int64_t MAX_FUTURE_BLOCK_TIME_DGW = 180;
-static const int64_t MAX_FUTURE_BLOCK_TIME_DALGO = 180;
+// increase more security against timestamp manipulation
+static const int64_t MAX_FUTURE_BLOCK_TIME = 60; 
+static const int64_t MAX_FUTURE_BLOCK_TIME_DGW = 60;
+static const int64_t MAX_FUTURE_BLOCK_TIME_DALGO = 60;
 
 /**
  * Timestamp window used as a grace period by code that compares external
