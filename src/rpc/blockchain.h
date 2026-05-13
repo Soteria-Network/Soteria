091898d7aefc27703f86ed97e24af50863a55a9b
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-05-13 22:24:02 +0300

    Update blockchain.h

--- src/rpc/blockchain.h
+++ src/rpc/blockchain.h
@@ -1,16 +1,17 @@
 // Copyright (c) 2011-2017 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Raven Core developers
-// Copyright (c) 2025 The Soteria Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2025-present The Soteria Core developers
 
 #ifndef SOTERIA_RPC_BLOCKCHAIN_H
 #define SOTERIA_RPC_BLOCKCHAIN_H
 
+#include <primitives/block.h>
+#include <validation.h> // for cs_main
+
 class CBlock;
 class CBlockIndex;
 class UniValue;
-#include <validation.h> // for cs_main
+
 /**
  * Get the difficulty of the net wrt to the given block index, or the chain tip if
  * not provided.
@@ -23,7 +24,7 @@ double GetDifficulty(const CBlockIndex* blockindex);
 double GetDifficulty(POW_TYPE powType);
 
 /** Callback for when block tip changed. */
-void RPCNotifyBlockChange(bool ibd, const CBlockIndex *);
+void RPCNotifyBlockChange(bool ibd, const CBlockIndex*);
 
 /** Block description to JSON */
 UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDetails = false) LOCKS_EXCLUDED(cs_main);
