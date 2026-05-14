b06beb85c272b564c35196e2e1e2f6679e6e55b0
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-05-14 11:09:47 +0300

    make clear() a Qt slot

--- src/qt/restrictedassignqualifier.h
+++ src/qt/restrictedassignqualifier.h
@@ -1,16 +1,14 @@
 // Copyright (c) 2011-2016 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Raven Core developers
-// Copyright (c) 2025 The Soteria Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2025-present The Soteria Core developers
 
 #ifndef SOTERIA_QT_ASSIGNQUALIFIER_H
 #define SOTERIA_QT_ASSIGNQUALIFIER_H
 
 #include "amount.h"
 
-#include <QWidget>
 #include <QMenu>
+#include <QWidget>
 #include <memory>
 
 class ClientModel;
@@ -22,8 +20,9 @@ class QCompleter;
 class AssetFilterProxy;
 
 
-namespace Ui {
-    class AssignQualifier;
+namespace Ui
+{
+class AssignQualifier;
 }
 
 QT_BEGIN_NAMESPACE
@@ -36,11 +35,11 @@ class AssignQualifier : public QWidget
     Q_OBJECT
 
 public:
-    explicit AssignQualifier(const PlatformStyle *_platformStyle, QWidget *parent = 0);
+    explicit AssignQualifier(const PlatformStyle* _platformStyle, QWidget* parent = 0);
     ~AssignQualifier();
 
-    void setClientModel(ClientModel *clientModel);
-    void setWalletModel(WalletModel *walletModel);
+    void setClientModel(ClientModel* clientModel);
+    void setWalletModel(WalletModel* walletModel);
     void showOutOfSyncWarning(bool fShow);
     Ui::AssignQualifier* getUI();
     bool eventFilter(QObject* object, QEvent* event);
@@ -49,16 +48,17 @@ public:
     void showWarning(QString string, bool failure = true);
     void hideWarning();
 
-    AssetFilterProxy *assetFilterProxy;
+    AssetFilterProxy* assetFilterProxy;
     QCompleter* completer;
 
-    void clear();
-
 private:
-    Ui::AssignQualifier *ui;
-    ClientModel *clientModel;
-    WalletModel *walletModel;
-    const PlatformStyle *platformStyle;
+    Ui::AssignQualifier* ui;
+    ClientModel* clientModel;
+    WalletModel* walletModel;
+    const PlatformStyle* platformStyle;
+
+public Q_SLOTS:
+    void clear();
 
 private Q_SLOTS:
     void check();
