8fbf3329a33a521ac6ac45fde4af4c53939ed1b6
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-05-14 11:17:30 +0300

    Update overviewpage.h

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -1,17 +1,15 @@
 // Copyright (c) 2011-2016 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Raven Core developers
-// Copyright (c) 2025 The Soteria Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2025-present The Soteria Core developers
 
 #ifndef SOTERIA_QT_OVERVIEWPAGE_H
 #define SOTERIA_QT_OVERVIEWPAGE_H
 
 #include "amount.h"
 
+#include <QMenu>
 #include <QSortFilterProxyModel>
 #include <QWidget>
-#include <QMenu>
 #include <memory>
 
 #include <QtNetwork/QNetworkAccessManager>
@@ -26,8 +24,9 @@ class AssetFilterProxy;
 
 class AssetViewDelegate;
 
-namespace Ui {
-    class OverviewPage;
+namespace Ui
+{
+class OverviewPage;
 }
 
 QT_BEGIN_NAMESPACE
@@ -40,36 +39,36 @@ class OverviewPage : public QWidget
     Q_OBJECT
 
 public:
-    explicit OverviewPage(const PlatformStyle *platformStyle, QWidget *parent = 0);
+    explicit OverviewPage(const PlatformStyle* platformStyle, QWidget* parent = 0);
     ~OverviewPage();
 
-    void setClientModel(ClientModel *clientModel);
-    void setWalletModel(WalletModel *walletModel);
+    void setClientModel(ClientModel* clientModel);
+    void setWalletModel(WalletModel* walletModel);
     void showOutOfSyncWarning(bool fShow);
     void showAssets();
 
-    bool eventFilter(QObject *object, QEvent *event);
-    void openDataForAsset(const QModelIndex &index, bool forceANS);
+    bool eventFilter(QObject* object, QEvent* event);
+    void openDataForAsset(const QModelIndex& index, bool forceANS);
 
     void setPriceBalance();
 
 public Q_SLOTS:
-            void getPriceInfo();
-            void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
-                            const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
+    void getPriceInfo();
+    void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
+ const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
 
-    Q_SIGNALS:
-            void transactionClicked(const QModelIndex &index);
-    void assetSendClicked(const QModelIndex &index);
-    void assetIssueSubClicked(const QModelIndex &index);
-    void assetIssueUniqueClicked(const QModelIndex &index);
-    void assetReissueClicked(const QModelIndex &index);
+Q_SIGNALS:
+    void transactionClicked(const QModelIndex& index);
+    void assetSendClicked(const QModelIndex& index);
+    void assetIssueSubClicked(const QModelIndex& index);
+    void assetIssueUniqueClicked(const QModelIndex& index);
+    void assetReissueClicked(const QModelIndex& index);
     void outOfSyncWarningClicked();
 
 private:
-    Ui::OverviewPage *ui;
-    ClientModel *clientModel;
-    WalletModel *walletModel;
+    Ui::OverviewPage* ui;
+    ClientModel* clientModel;
+    WalletModel* walletModel;
     CAmount currentBalance;
     CAmount currentUnconfirmedBalance;
     CAmount currentImmatureBalance;
@@ -77,31 +76,31 @@ private:
     CAmount currentWatchUnconfBalance;
     CAmount currentWatchImmatureBalance;
 
-    TxViewDelegate *txdelegate;
+    TxViewDelegate* txdelegate;
     std::unique_ptr<TransactionFilterProxy> filter;
     std::unique_ptr<AssetFilterProxy> assetFilter;
 
-    AssetViewDelegate *assetdelegate;
-    QMenu *contextMenu;
-    QAction *sendAction;
-    QAction *issueSub;
-    QAction *issueUnique;
-    QAction *reissue;
-    QAction *openURL;
-    QAction *viewANS;
-    QAction *copyHashAction;
+    AssetViewDelegate* assetdelegate;
+    QMenu* contextMenu;
+    QAction* sendAction;
+    QAction* issueSub;
+    QAction* issueUnique;
+    QAction* reissue;
+    QAction* openURL;
+    QAction* viewANS;
+    QAction* copyHashAction;
 
     /** SOTER START */
-    QTimer *pricingTimer;
+    QTimer* pricingTimer;
     QNetworkAccessManager* networkManager;
     QNetworkRequest* request;
     /** SOTER END */
 
 private Q_SLOTS:
     void updateDisplayUnit();
-    void handleTransactionClicked(const QModelIndex &index);
-    void handleAssetRightClicked(const QModelIndex &index);
-    void updateAlerts(const QString &warnings);
+    void handleTransactionClicked(const QModelIndex& index);
+    void handleAssetRightClicked(const QModelIndex& index);
+    void updateAlerts(const QString& warnings);
     void updateWatchOnlyLabels(bool showWatchOnly);
     void handleOutOfSyncWarningClicks();
     void assetSearchChanged();
