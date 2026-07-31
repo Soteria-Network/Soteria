ace2e22e5bdad0d94a41d737edee35d69b79e8da
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-07-31 13:23:23 +0300

    add paperwallet dlg

--- src/qt/utilitydialog.h
+++ src/qt/utilitydialog.h
@@ -1,20 +1,43 @@
 // Copyright (c) 2011-2016 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Raven Core developers
-// Copyright (c) 2025 The Soteria Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2025-2026 The Soteria Core developer
+
 
 #ifndef SOTERIA_QT_UTILITYDIALOG_H
 #define SOTERIA_QT_UTILITYDIALOG_H
 
 #include <QDialog>
 #include <QObject>
+#include "walletmodel.h"
 
 class SoteriaGUI;
 
 namespace Ui {
     class HelpMessageDialog;
+    class PaperWalletDialog;
 }
+
+/** "Paper Wallet" dialog box */
+class PaperWalletDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit PaperWalletDialog(QWidget *parent);
+    ~PaperWalletDialog();
+
+    void setModel(WalletModel *model);
+
+private:
+    Ui::PaperWalletDialog *ui;
+    WalletModel *model;
+    static const int PAPER_WALLET_READJUST_LIMIT = 20;
+    static const int PAPER_WALLET_PAGE_MARGIN = 50;
+
+private Q_SLOTS:
+    void on_getNewAddress_clicked();
+    void on_printButton_clicked();
+};
 
 /** "Help message" dialog box */
 class HelpMessageDialog : public QDialog
