06fec6a6add3e95a30610d657d722d9e6bae824c
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-08-04 13:42:52 +0300

    add PQ block weight limit

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -1,9 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2011-2016 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Raven Core developers
-// Copyright (c) 2025 The Soteria Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2025-2026 The Soteria Core developer
 
 #ifndef SOTERIA_CONSENSUS_CONSENSUS_H
 #define SOTERIA_CONSENSUS_CONSENSUS_H
@@ -17,21 +15,30 @@ static constexpr unsigned int MAX_BLOCK_SERIALIZED_SIZE = 3000000;
 /** The maximum allowed weight for a block, see BIP 141 (network rule) */
 static constexpr unsigned int MAX_BLOCK_WEIGHT = 3000000;
 
-/** The maximum allowed weight for a block, after RIP 2 (network rule) */
-//static constexpr unsigned int MAX_BLOCK_WEIGHT_RIP2 = 4000000;
-// Tighter caps can push fees up under load; looser caps dampen spikes but risk “wasted” empty slots.
-/** The maximum allowed size for a serialized block, in bytes after RIP 2(only for buffer size limits) */
-//static constexpr unsigned int MAX_BLOCK_SERIALIZED_SIZE_RIP2 = 4000000;
+/** RIP-25: Phase 1 PQ block weight limit (8 MWU) */
+static const unsigned int MAX_BLOCK_WEIGHT_RIP25_PHASE1 = 8000000;
+/** RIP-25: Phase 1 PQ block serialized size limit */
+static const unsigned int MAX_BLOCK_SERIALIZED_SIZE_RIP25_PHASE1 = 8000000;
+/** RIP-25: Phase 2 PQ block weight limit (10 MWU) */
+static const unsigned int MAX_BLOCK_WEIGHT_RIP25_PHASE2 = 10000000;
+/** RIP-25: Phase 2 PQ block serialized size limit */
+static const unsigned int MAX_BLOCK_SERIALIZED_SIZE_RIP25_PHASE2 = 10000000;
 
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static constexpr int64_t MAX_BLOCK_SIGOPS_COST = 240000; // SC support // MAX_BLOCK_WEIGHT/50
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
-static constexpr int COINBASE_MATURITY = 4200; // 60Kblocks/Epoch =8mb ~16h
+static constexpr int COINBASE_MATURITY = 4200;
 
 /** Timestamp at which the UAHF starts. */
-static constexpr uint32_t DEFAULT_UAHF_START_TIME = 2147483647; // 1760052990; until the release of Electron wallet
+static constexpr uint32_t DEFAULT_UAHF_START_TIME = 2147483647; // 1760052990;Date: Tuesday, January 19, 2038, at 03:14:07 UTC
 
 static constexpr int WITNESS_SCALE_FACTOR = 4;
+
+/** RIP-25: Maximum witness stack element size for PQ (witness v2) programs */
+static const unsigned int MAX_PQ_WITNESS_ELEMENT_SIZE = 4096;
+
+/** RIP-25: PQ witness discount scale factor (8x base, PQ witness at 1/8 weight) */
+static const int PQ_WITNESS_SCALE_FACTOR = 8;
 
 static constexpr size_t MIN_TRANSACTION_WEIGHT = WITNESS_SCALE_FACTOR * 60; // 60 is the lower bound for the size of a valid serialized CTransaction
 static constexpr size_t MIN_SERIALIZABLE_TRANSACTION_WEIGHT = WITNESS_SCALE_FACTOR * 10; // 10 is the lower bound for the size of a serialized CTransaction
@@ -47,7 +54,7 @@ UNUSED_VAR static bool fTransferScriptIsActive = false;
 /** Enable missing activation flags. */
 UNUSED_VAR static bool fEnforcedValuesIsActive = true; // Always return true for enforced values.
 UNUSED_VAR static bool fCheckCoinbaseAssetsIsActive = true; // Always return true for coinbase asset checks.
-
+UNUSED_VAR static bool fPQHybridIsActive = false;
 unsigned int GetMaxBlockWeight();
 unsigned int GetMaxBlockSerializedSize();
 
