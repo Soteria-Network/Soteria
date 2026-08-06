df2082d664bc63a9c65a71a2e285e942bc51b516
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-08-06 18:41:50 +0300

    allow liboqs build on unsupported architectures

--- depends/packages/liboqs.mk
+++ depends/packages/liboqs.mk
@@ -2,27 +2,37 @@ package=liboqs
 $(package)_version=0.12.0
 $(package)_download_path=https://github.com/open-quantum-safe/liboqs/archive/refs/tags/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=TODO_REPLACE_WITH_ACTUAL_HASH
+$(package)_sha256_hash=df999915204eb1eba311d89e83d1edd3a514d5a07374745d6a9e5b2dd0d59c08
 $(package)_dependencies=
-$(package)_patches=
+#$(package)_patches=
 
 define $(package)_set_vars
   $(package)_config_opts=-DOQS_BUILD_ONLY_LIB=ON
   $(package)_config_opts+=-DOQS_MINIMAL_BUILD="SIG_ml_dsa_44"
   $(package)_config_opts+=-DOQS_USE_OPENSSL=OFF
   $(package)_config_opts+=-DBUILD_SHARED_LIBS=OFF
-  $(package)_config_opts+=-DCMAKE_INSTALL_PREFIX=$(host_prefix)
+#  $(package)_config_opts+=-DCMAKE_INSTALL_PREFIX=$(host_prefix)
   $(package)_config_opts+=-DOQS_DIST_BUILD=ON
+  $(package)_config_opts+=-DOQS_PERMIT_UNSUPPORTED_ARCHITECTURE=ON
+  $(package)_config_opts+=-DCMAKE_SYSTEM_PROCESSOR=$(host_arch)
+  $(package)_config_opts_darwin+=-DOQS_DIST_BUILD=OFF
+  $(package)_config_opts_darwin+=-DOQS_USE_AVX2_INSTRUCTIONS=OFF
+  $(package)_config_opts_darwin+=-DOQS_USE_AVX_INSTRUCTIONS=OFF
+  $(package)_config_opts_darwin+=-DOQS_USE_SSE2_INSTRUCTIONS=OFF
 endef
 
 define $(package)_config_cmds
-  cmake -S . -B build $($(package)_config_opts)
+  $($(package)_cmake) -S . -B .
 endef
 
 define $(package)_build_cmds
-  cmake --build build --parallel
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
-  cmake --install build --prefix $($(package)_staging_prefix_dir)
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm -rf share
 endef
