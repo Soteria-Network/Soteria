6a5193cdd1689339f5408e27cf0f4cfb36b7234d
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-08-22 11:12:31 +0300

    Implement IsPQWitnessDiscountActive() and GetMaxBlockWeightForPrev()

--- src/consensus/consensus.cpp
+++ src/consensus/consensus.cpp
@@ -4,24 +4,31 @@
 // Copyright (c) 2025-2026 The Soteria Core developer
 
 #include "consensus.h"
-#include <validation.h>
 
 unsigned int GetMaxBlockWeight()
 {
-    // RIP-25: Phase 1 PQ block weight increase
-    if (fPQHybridIsActive)
-        return MAX_BLOCK_WEIGHT_RIP25_PHASE1;
-
-    // Block weight for when assets weren't activated
-    return MAX_BLOCK_WEIGHT;
+    return MAX_BLOCK_WEIGHT_RIP25_PHASE2;
 }
 
 unsigned int GetMaxBlockSerializedSize()
 {
-    // RIP-25: Phase 1 PQ block serialized size increase
-    if (fPQHybridIsActive)
-        return MAX_BLOCK_SERIALIZED_SIZE_RIP25_PHASE1;
+    return MAX_BLOCK_SERIALIZED_SIZE_RIP25_PHASE2;
+}
 
-    // Block serialized size for when assets weren't activated
-    return MAX_BLOCK_SERIALIZED_SIZE;
+bool IsPQWitnessDiscountActive(const CBlockIndex* pindexPrev, const Consensus::ConsensusParams& consensusParams)
+{
+    // If we later use VersionBitsState, we will replace this with that call.
+    // For now, we will trust the nPQHybridEnabled flag from chainparams.
+    return consensusParams.nPQHybridEnabled;
+}
+
+unsigned int GetMaxBlockWeightForPrev(const CBlockIndex* pindexPrev, const Consensus::ConsensusParams& consensusParams)
+{
+    // Before PQ activation, we will keep the legacy 3M limit.
+    if (!IsPQWitnessDiscountActive(pindexPrev, consensusParams))
+        return MAX_BLOCK_WEIGHT;
+
+    // Once active, use the phase2 structural ceiling.
+    // TODO: Add phase1 logic if we have an intermediate activation phase.
+    return MAX_BLOCK_WEIGHT_RIP25_PHASE2;
 }
