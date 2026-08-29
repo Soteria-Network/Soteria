4fdb83bb5423ed174ab6f47083b47555ecae0f97
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-08-30 02:36:56 +0300

    update the version

--- depends/packages/liboqs.mk
+++ depends/packages/liboqs.mk
@@ -1,24 +1,28 @@
 package=liboqs
-$(package)_version=0.12.0
+$(package)_version=0.16.0
 $(package)_download_path=https://github.com/open-quantum-safe/liboqs/archive/refs/tags/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=df999915204eb1eba311d89e83d1edd3a514d5a07374745d6a9e5b2dd0d59c08
+$(package)_sha256_hash=162d5b510518ee5f285f82fa1f16402a885176e818bf1b1a4c3c91c9a2f01eae
 $(package)_dependencies=
-#$(package)_patches=
 
 define $(package)_set_vars
   $(package)_config_opts=-DOQS_BUILD_ONLY_LIB=ON
   $(package)_config_opts+=-DOQS_MINIMAL_BUILD="SIG_ml_dsa_44"
   $(package)_config_opts+=-DOQS_USE_OPENSSL=OFF
   $(package)_config_opts+=-DBUILD_SHARED_LIBS=OFF
-#  $(package)_config_opts+=-DCMAKE_INSTALL_PREFIX=$(host_prefix)
   $(package)_config_opts+=-DOQS_DIST_BUILD=ON
+  $(package)_config_opts_arm=-DCMAKE_SYSTEM_PROCESSOR=armv7
+  $(package)_config_opts_x86_64=-DCMAKE_SYSTEM_PROCESSOR=x86_64
+  $(package)_config_opts_mingw32=-DOQS_DIST_BUILD=OFF
   $(package)_config_opts+=-DOQS_PERMIT_UNSUPPORTED_ARCHITECTURE=ON
   $(package)_config_opts+=-DCMAKE_SYSTEM_PROCESSOR=$(host_arch)
   $(package)_config_opts_darwin+=-DOQS_DIST_BUILD=OFF
   $(package)_config_opts_darwin+=-DOQS_USE_AVX2_INSTRUCTIONS=OFF
   $(package)_config_opts_darwin+=-DOQS_USE_AVX_INSTRUCTIONS=OFF
   $(package)_config_opts_darwin+=-DOQS_USE_SSE2_INSTRUCTIONS=OFF
+  # The darwin CC wrapper starts with 'env', which CMake's ASM detection
+  # misreads as the compiler itself; name a real assembler driver for XKCP.
+  $(package)_config_opts_darwin=-DCMAKE_ASM_COMPILER=$(clang_prog) -DCMAKE_ASM_COMPILER_TARGET=$(host)  
 endef
 
 define $(package)_config_cmds
