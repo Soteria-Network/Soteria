f881f97abd50cb424d940ef1cd3cc3f6ee5754ac
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-09-11 14:59:04 +0300

    Add an explicit return false for release builds

--- src/qt/peertablemodel.cpp
+++ src/qt/peertablemodel.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) 2011-2016 The Bitcoin Core developers
 // Copyright (c) 2017-2019 The Raven Core developers
-// Copyright (c) 2025 The Soteria Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2025-2026 The Soteria Core developer
 
 #include "peertablemodel.h"
 #include <map>
@@ -43,6 +41,7 @@ bool NodeLessThan::operator()(const CNodeCombinedStats &left, const CNodeCombine
         return pLeft->nRecvBytes < pRight->nRecvBytes;
     } // no default case, so the compiler can warn about missing cases
     assert(false);
+    return false; // unreachable in debug, but silences -Wreturn-type
 }
 
 // private implementation
